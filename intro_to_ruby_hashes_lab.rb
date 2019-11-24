d60d30e0f28e41e28dca8f2b59e8d34092d1bd32
Felicia Smith <smithfn1012@gmail.com>
2019-11-24 21:49:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -69,16 +69,16 @@ def monopoly_with_third_tier
         :four_pieces_owned => 200
        },
        :names => {
-         :reading_railroad => {
+         :reading_railroad=> {
            "mortgage_value" => "$100"
            },
-         :b_and_o_railroad => {
+         :b_and_o_railroad=> {
            "mortgage_value" => "$100"
          },
-         :shortline => {
+         :shortline=> {
            "mortgage_value" => "$100"
          },
-         :pennsylvania_railroad => {
+         :pennsylvania_railroad=> {
            "mortgage_value" => "$100"
          }
       }
