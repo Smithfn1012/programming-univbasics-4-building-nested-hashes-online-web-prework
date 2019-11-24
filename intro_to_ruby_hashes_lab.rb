c6d5046553c2c3cdde161ab5318b67240770d8e1
Felicia Smith <smithfn1012@gmail.com>
2019-11-24 22:24:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,16 +45,9 @@ def monopoly_with_third_tier
     }
 }
 end
-
-
-def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
   
 
-def monopoly_with_third_tier
+def monopoly_with_fourth_tier
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
@@ -95,5 +88,5 @@ def monopoly_with_third_tier
 end
 
 end
-[:railroads][:names]["mortgage_value"].each.to eq("$" + 100)
+
 
