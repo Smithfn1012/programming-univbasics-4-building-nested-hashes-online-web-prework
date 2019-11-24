b170f02aea76e8a023ca2d06e9ac843ce4cb73ab
Felicia Smith <smithfn1012@gmail.com>
2019-11-24 22:04:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -70,7 +70,7 @@ def monopoly_with_third_tier
        },
        :names => {
          :reading_railroad=> {
-           "mortgage_value" => "$100"
+           "mortgage_value" => $100
            },
          :b_and_o_railroad=> {
            "mortgage_value" => "$200"
